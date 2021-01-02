a4fc3a0749e622ffc57ffd5781a4578688bed7d4
Harshan-Sarkar <74543068+Harshan-Sarkar@users.noreply.github.com>
2021-01-02 13:52:59 +0530

    Add files via upload

---
+++ Sam_code.py
@@ -0,0 +1,50 @@
+h = "h"
+e = "e"
+l = "l"
+l = "l"
+o = "o"
+w = "w"
+o = "o"
+r = "r"
+l = "l"
+d = "d"
+def print_hello_world():
+    S = h + e
+    A = S + l
+    M = A + l
+    U = M + o
+    E = U + " "
+    L = E + w
+    M = L + o
+    I = M + r
+    L = I + l
+    L = L + d
+    E = None
+    R = None
+    print(L)
+print_hello_world()
+
+
+h = "h"
+e = "e"
+l = "l"
+l = "l"
+o = "o"
+w = "w"
+o = "o"
+r = "r"
+l = "l"
+d = "d"
+print(h, end= "")
+print(e, end= "")
+print(l, end= "")
+print(l, end= "")
+print(o, end= "")
+print(" ", end= "")
+print(w, end= "")
+print(o, end= "")
+print(r, end= "")
+print(l, end= "")
+print(d)
+
+print("hello world")
